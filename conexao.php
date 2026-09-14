c8229c5fca8d0fb9019f9d5b7fd3df5ebfade68c
Cazé Portela <carloshenrik0909@gmail.com>
2026-09-14 12:23:06 -0300

    dockers

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$usuario = 'root';
-$senha = '';
-$banco = 'phmetro';
+$host = getenv('DB_HOST') ?: 'localhost';
+$usuario = getenv('DB_USER') ?: 'root';
+$senha = getenv('DB_PASS') ?: '';
+$banco = getenv('DB_NAME') ?: 'phmetro';
 
 date_default_timezone_set('America/Sao_Paulo');
 
